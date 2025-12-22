1a36f23db072fd3f41495c01e2487535faaf395d
mahamed.msh <mahamedsheik243@gmail.com>
2025-12-22 21:13:38 +0300

    exercise 1

---
+++ exercise 1/main.js
@@ -0,0 +1,11 @@
+let x=7, y=3;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+console.log(x++ , y++);
+console.log(x , y);
+
+console.log(++x , ++y);
