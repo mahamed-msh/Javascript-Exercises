a492b131e2c50bd5c2d3cf24207ab52e6f2fd78a
mahamed.msh <mahamedsheik243@gmail.com>
2025-12-24 22:46:40 +0300

    exercise 7

---
+++ exercise 7/main.js
@@ -0,0 +1,21 @@
+let colors =['green','black','yellow'];
+
+console.log('Before added new color')
+console.log(colors)
+
+colors.push("blue");
+console.log('after added a new color at the ennd')
+console.log(colors)
+
+colors.shift()
+console.log('after deleted the first color')
+console.log(colors)
+
+
+
+
+
+
+
+
+
