dbdf8c57f8de1d68ad2196897620ab57455f3518
mahamed.msh <mahamedsheik243@gmail.com>
2025-12-25 20:52:13 +0300

    exercise 10

---
+++ exercise 10/main.js
@@ -0,0 +1,22 @@
+let Library =[
+
+    {
+        title : 'Financial Accounting',
+        author : 'Walter T.Harrison',
+        year : 2003
+    },
+
+    {
+        title : 'Managerial Accounting',
+        author : 'Ray H.Garrison',
+        year :  1989
+    },
+    {
+        title : 'Intermediaste Accounting',
+        author : 'Donald E.Kieso',
+        year : 1973
+    }
+]
+
+console.log(Library[0].title);
+console.log(Library[1].author);
