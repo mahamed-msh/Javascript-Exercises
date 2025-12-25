35c1e179018e33970e66d5f761e2f04e4796916c
mahamed.msh <mahamedsheik243@gmail.com>
2025-12-25 20:35:40 +0300

    exercise 9

---
+++ exercise 9/main.js
@@ -0,0 +1,14 @@
+let car ={
+    model : 'model y',
+    make : 'Tesla',
+    year : 2020,
+    start : ()=>{
+        console.log('The car has started')
+
+    }
+}
+
+console.log(car.model);
+console.log(car.make);
+console.log(car.year);
+car.start();
