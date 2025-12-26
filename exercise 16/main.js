2b248562b192e558a80c9da4eafeb3d5e73e5a87
mahamed.msh <mahamedsheik243@gmail.com>
2025-12-26 18:37:01 +0300

    exercise 16 & 17

--- exercise 16/main.js
+++ exercise 16/main.js
@@ -1,17 +1,8 @@
-let score = prompt('enter your score');
+let score = 89;
 
-do{
-   {
-  score = prompt('please enter a number less than or equal to 100')
-  score++
-}
-}
-while(score>100)
-
-if (score>=50){
+ if (score>=50){
    console.log('You Passed!')
 }
-
 
 else{
    console.log('you Failed!')
---
+++ exercise 17/main.js
@@ -0,0 +1,14 @@
+let tempreture = 20;
+
+if(tempreture<0){
+   console.log("Very cold")
+}
+else if(tempreture>=0 && tempreture<=15){
+   console.log("Cold")
+}
+else if(tempreture>15 && tempreture<=25){
+   console.log("Warm")
+}
+else {
+   console.log('Hot')
+}
