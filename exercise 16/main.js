73a92071a96f833c7cbac356188c51185cb15e3e
mahamed.msh <mahamedsheik243@gmail.com>
2025-12-26 18:05:44 +0300

    exercise 16

---
+++ exercise 16/main.js
@@ -0,0 +1,18 @@
+let score = prompt('enter your score');
+
+do{
+   {
+  score = prompt('please enter a number less than or equal to 100')
+  score++
+}
+}
+while(score>100)
+
+if (score>=50){
+   console.log('You Passed!')
+}
+
+
+else{
+   console.log('you Failed!')
+}
