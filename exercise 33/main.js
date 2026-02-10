0e227b79d3204feabaa152d0999ca6788d7ff4f1
mahamed.msh <mahamedsheik243@gmail.com>
2026-02-10 07:13:08 +0300

    exercise 33

---
+++ exercise 33/main.js
@@ -0,0 +1,9 @@
+const header = document.querySelector("#h1");
+const paragraphs = document.querySelector(".all")
+const btn = document.querySelector("button");
+
+function changeElement(){
+    header.textContent= 'Exercise 33';
+}
+
+
